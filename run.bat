05591746847690e8323ec1aea068f89d6626e882
wanyi <95110989+red-maple-leaf-master@users.noreply.github.com>
2025-06-25 16:38:29 +0800

    fix: 修改打包配置

--- run.bat
+++ run.bat
@@ -1,1 +1,1 @@
-jpackage --name envLauncher --type app-image -m top.oneyi.jdktool/top.oneyi.jdktool.MainApp --runtime-image .\target\app\ --icon src/main/resources/icons/favicon.ico
+jpackage --name envLauncher --type app-image -m top.oneyi.jdktool/top.oneyi.envLauncher.MainApp --runtime-image .\target\app\ --icon src/main/resources/icons/favicon.ico
