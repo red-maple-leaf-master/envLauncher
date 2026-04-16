9e5772dd22b2dbf6c8b20f5d1bfb61a21a626db5
wanyi <1362187574@qq.com>
2026-04-16 13:33:41 +0800

    fix: AI生成-更新JDK镜像版本映射

--- src/main/java/top/oneyi/envLauncher/config/JDKVersionConfig.java
+++ src/main/java/top/oneyi/envLauncher/config/JDKVersionConfig.java
@@ -10,10 +10,10 @@ public class JdkVersionConfig {
     private static final Map<String, String> VERSION_TO_ARTIFACT_PATH = new HashMap<>();
 
     static {
-        VERSION_TO_ARTIFACT_PATH.put("8", "8/jdk/x64/windows/OpenJDK8U-jdk_x64_windows_hotspot_8u452b09.zip");
-        VERSION_TO_ARTIFACT_PATH.put("11", "11/jdk/x64/windows/OpenJDK11U-jdk_x64_windows_hotspot_11.0.27_6.zip");
-        VERSION_TO_ARTIFACT_PATH.put("17", "17/jdk/x64/windows/OpenJDK17U-jdk_x64_windows_hotspot_17.0.15_6.zip");
-        VERSION_TO_ARTIFACT_PATH.put("21", "21/jdk/x64/windows/OpenJDK21U-jdk_x64_windows_hotspot_21.0.7_6.zip");
+        VERSION_TO_ARTIFACT_PATH.put("8", "8/jdk/x64/windows/OpenJDK8U-jdk_x64_windows_hotspot_8u482b08.zip");
+        VERSION_TO_ARTIFACT_PATH.put("11", "11/jdk/x64/windows/OpenJDK11U-jdk_x64_windows_hotspot_11.0.30_7.zip");
+        VERSION_TO_ARTIFACT_PATH.put("17", "17/jdk/x64/windows/OpenJDK17U-jdk_x64_windows_hotspot_17.0.18_8.zip");
+        VERSION_TO_ARTIFACT_PATH.put("21", "21/jdk/x64/windows/OpenJDK21U-jdk_x64_windows_hotspot_21.0.10_7.zip");
     }
 
     public static String getArtifactPath(String version) {
